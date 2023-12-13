ad620e994eabba2333914a977a5a24ae86cbd640
Guillermo Croppi <guillermocroppi@gmail.com>
2023-12-13 14:11:18 -0300

    Refactor userController.js to include admin functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,23 @@ const agenda = require('../services/agenda');
 
 exports.list = async function (req, res) {
 	try {
+		const user = req.user;
 		const page = req.query.page || 1;
 		const limit = req.query.limit || 10;
+		let includeDeleted = false;
+		let public = true;
+		let extraQuery = null;
+		// if it is an admin
+		if (user && user.role === 'admin') {
+			public = false;
+			// check req.query.includeDeleted
+			if (req.query.includeDeleted && req.query.includeDeleted === 'true') {
+				includeDeleted = true;
+			}
+		}
+
 		// get the users
-		const output = await UserHelper.listUsers(page, limit, true);
+		const output = await UserHelper.listUsers(page, limit, extraQuery, public, includeDeleted);
 		return res.json(output);
 
 	} catch (error) {
@@ -18,15 +31,24 @@ exports.list = async function (req, res) {
 }
 
 exports.me = async function (req, res) {
-	const user = await User.findOne({ _id: req.user._id }, {
-		password: 0,
-		resetPasswordToken: 0,
-		resetPasswordExpires: 0,
-		createdAt: 0,
-		updatedAt: 0,
-		__v: 0
-	});
-	return res.status(200).json(user);
+	try {
+		const userId = req.user._id;
+		const user = await User.findById(userId, {
+			password: false,
+			resetPasswordToken: false,
+			resetPasswordExpires: false,
+			createdAt: false,
+			updatedAt: false,
+			__v: false
+		}).populate({
+			path: 'country',
+			select: '_id name code emoji unicode image'
+		})
+		return res.status(200).json(user);
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ message: error.message })
+	}
 };
 
 /**
@@ -35,20 +57,33 @@ exports.me = async function (req, res) {
  */
 exports.get = async function (req, res) {
 	try {
-		const id = req.params.id;
+		const userId = req.params.userId;
+		const userLogged = req.user;
+		let querySelect = {
+			password: false,
+			resetPasswordToken: false,
+			resetPasswordExpires: false,
+			isVerified: false,
+			createdAt: false,
+			updatedAt: false,
+			__v: false
+		}
+		const querySelectForAdmins = {
+			password: false,
+			__v: false
+		}
+		
+		let queryProjection;
 
-		const user = await User.findById(id,{
-			password: 0,
-			resetPasswordToken: 0,
-			resetPasswordExpires: 0,
-			createdAt: 0,
-			updatedAt: 0,
-			__v: 0
-		});
+		if(userLogged && userLogged.role.includes['admin']){
+			queryProjection = querySelectForAdmins;
+		}
+
+		const user = await User.findById(userId, querySelect);
 
 		if (!user) return res.status(401).json({ message: 'User does not exist' });
 
-		return res.status(200).json({ user });
+		return res.status(200).json(user);
 	} catch (error) {
 		return res.status(500).json({ message: error.message })
 	}
@@ -109,7 +144,7 @@ exports.update = async function (req, res) {
 		const id = req.params.id;
 		const userId = req.user._id;
 
-		//Make sure the passed id is that of the logged in user
+		// Make sure the passed id is that of the logged in user
 		if (userId.toString() !== id.toString()) return res.status(401).json({ message: "Sorry, you don't have the permission to upd this data." });
 
 		const user = await User.findByIdAndUpdate(id, { $set: update }, { new: true });
