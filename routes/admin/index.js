03a1dd9d61d2d4d08cb17635ac7b28ad9f55c122
Guillermo Croppi <guillermocroppi@gmail.com>
2023-12-13 14:10:55 -0300

    Add admin routes

---
+++ routes/admin/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { check, param, body, query } = require('express-validator');
+const constants = require('../../services/constants');
+const authenticate = require('../../middlewares/authenticate');
+const exists = require('../../middlewares/exists');
+const validate = require('../../middlewares/validate');
+
+const UserController = require('../../controllers/userController');
+
+// initialize router
+const router = express.Router({mergeParams: true});
+
+// -----------------------------------------------
+// BASE     /admin
+// -----------------------------------------------
+// GET 		/admin/users
+// GET 		/admin/users/:userId
+// -----------------------------------------------
+
+// GET 		/admin/users
+router.get('/users',
+  [
+    query('page').optional().isInt({ min: 1 }).withMessage('Page must be an integer greater than 0'),
+    query('limit').optional().isInt({ min: 10 }).withMessage('Limit must be an integer greater than 0'),
+    query('includeDeleted').optional().isBoolean().withMessage('Include deleted must be a boolean'),
+  ],
+  validate,
+  authenticate(constants.ROLES.ADMINISTRATOR),
+  UserController.list
+)
+
+// GET 		/admin/users/:userId
+router.get('/users/:userId',
+  [
+    param('userId').isMongoId().withMessage('Invalid User ID'),
+  ],
+  validate,
+  exists.user,
+  authenticate(constants.ROLES.ADMINISTRATOR),
+  UserController.get
+)
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ const testRoutes = require('./test');
 const miscRoutes = require('./misc');
 const projectsRoutes = require('./projects');
 const usersRoutes = require('./users');
-const authenticate = require('../middlewares/authenticate');
+const adminRoutes = require('./admin');
+// const authenticate = require('../middlewares/authenticate');
 
 module.exports = app => {
     app.get('/', (req, res) => {
         res.status(200).json({message: "Welcome to the API"});
     });
     app.use('/auth', authRoutes);
+    app.use('/admin', adminRoutes);
     app.use('/users', usersRoutes);
     app.use('/projects', projectsRoutes);
     app.use('/misc', miscRoutes);
