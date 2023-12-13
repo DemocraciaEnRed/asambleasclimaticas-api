cb270049230d68fbcd145bea6a93cbd66861410a
Guillermo Croppi <guillermocroppi@gmail.com>
2023-12-13 14:11:02 -0300

    Refactor listUsers function in usersHelper.js

--- helpers/usersHelper.js
+++ helpers/usersHelper.js
@@ -1,8 +1,9 @@
 
 const User = require('../models/user');
 
-const publicProjection = {
+const publicSelect = {
   __v: false,
+  email: false,
   password: false,
   resetPasswordToken: false,
   resetPasswordExpires: false,
@@ -12,13 +13,34 @@ const publicProjection = {
   isVerified: false
 };
 
-exports.listUsers = async (page = 1, limit = 10, public) => {
+const privateSelect = {
+  password: false,
+  __v: false,
+};
+
+const queryForEnabledUsers = {
+  deletedAt: {
+    $or: [null, { $exists: false }]
+  }
+}
+
+const queryForEveryUser = {}
+
+
+exports.listUsers = async (page = 1, limit = 10, extraQuery = null, public = true, includeDeleted = false) => {
   try {
+    let query = queryForEnabledUsers;
+    if(!includeDeleted) {
+      query = queryForEveryUser;
+    }
+    if(extraQuery) {
+      query = {...query, ...extraQuery};
+    }
     // get the users by page
-    const users = await User.find({}, public ? publicProjection : {}).skip((page - 1) * limit).limit(limit);
+    const users = await User.find(query, public ? publicSelect : privateSelect).populate('country').skip((page - 1) * limit).limit(limit);
 
     // get pagination metadata
-    const total = await User.countDocuments(); // get total of users
+    const total = await User.countDocuments(query);
     const pages = Math.ceil(total / limit); // round up to the next integer
     const nextPage = page < pages ? page + 1 : null; // if there is no next page, return null
     const prevPage = page > 1 ? page - 1 : null; // if there is no previous page, return null
