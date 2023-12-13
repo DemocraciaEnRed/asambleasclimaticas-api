413e38ce5d7f9a6c1dfc9d630e0da01a0f04df75
Guillermo Croppi <guillermocroppi@gmail.com>
2023-12-13 14:11:38 -0300

    Update user ID field in JWT middleware

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -4,22 +4,22 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 
 const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: process.env.JWT_SECRET
 };
 
 module.exports = passport => {
-    passport.use(
-        new JwtStrategy(opts, (jwt_payload, done) => {
-            // console.log(jwt_payload)
-            User.findById(jwt_payload.id)
-                .then(user => {
-                    if (user) return done(null, user);
-                    return done(null, false);
-                })
-                .catch(err => {
-                    return done(err, false, {message: 'Server Error'});
-                });
-        })
-    );
+	passport.use(
+		new JwtStrategy(opts, (jwt_payload, done) => {
+			// console.log(jwt_payload)
+			User.findById(jwt_payload._id)
+				.then(user => {
+					if (user) return done(null, user);
+					return done(null, false);
+				})
+				.catch(err => {
+					return done(err, false, { message: 'Server Error' });
+				});
+		})
+	);
 };
