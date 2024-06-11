1b3be3755d21859d9b120690134b9a6ae168e93e
Guillermo Croppi <guillermocroppi@gmail.com>
2024-06-11 12:19:31 -0300

    chore: Add graceful shutdown and app status monitoring

--- queue.js
+++ queue.js
@@ -4,6 +4,8 @@ const jobs = require('./jobs/index');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
+const fs = require('fs');
+const path = require('path');
 
 // Set up timezone argentina for dayjs
 dayjs.extend(utc)
@@ -26,3 +28,24 @@ agenda.define('send-mail', jobs.sendMail)
 // start agenda
 agenda.start();
 
+// ---------------------------------------------------
+
+async function graceful() {
+	await agenda.stop();
+  // write a file in the root of the directory that saves 1 if the app is not alive
+  fs.writeFileSync(path.join(__dirname, 'alive'), '1');
+	process.exit(0);
+}
+
+// every 5 secs, write a file in the root of the directory that saves 0 if the app is alive
+fs.writeFileSync(path.join(__dirname, 'alive'), '0');
+setInterval(() => {
+  fs.writeFileSync(path.join(__dirname, 'alive'), '0');
+}, 5 * 1000);
+
+process.on('SIGTERM', graceful);
+process.on('SIGINT', graceful);
+
+// on process kill, stop agenda
+process.on('exit', graceful);
+process.on('beforeExit', graceful);
